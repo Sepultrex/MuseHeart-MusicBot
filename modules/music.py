ff365a97244efc7f4209ca590ff5cf61c7786562
Ceo Of Sinop <65771082+Sepultrex@users.noreply.github.com>
2024-09-03 22:49:50 +0300

    69978e8

--- modules/music.py
+++ modules/music.py
@@ -5386,6 +5386,7 @@ class Music(commands.Cog):
                     return
 
                 user_data["lastfm"]["scrobble"] = not user_data["lastfm"]["scrobble"]
+                await self.bot.update_global_data(interaction.author.id, user_data, db_name=DBModel.users)
                 await interaction.edit_original_message(
                     embed=disnake.Embed(
                         description=f'**Scrobble/şarkı kaydı şu şekilde: {"açık" if user_data["lastfm"]["scrobble"] else "kapalı"} **',
